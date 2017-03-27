3b6053a6550cb405703c5ef42f30fafbfa0cb06b
Aluno <aluno@cesed.local>
2017-03-27 09:46:15 -0300

    Refazendo o codigo e criando a classe de testes

--- ListaSequencial/src/cesed/facisa/si/p3/listaSequencial/MeuArray.java
+++ ListaSequencial/src/cesed/facisa/si/p3/listaSequencial/MeuArray.java
@@ -1,19 +1,100 @@
 package cesed.facisa.si.p3.listaSequencial;
 
-public class MeuArray {
-	
-	
-	private Object [] objects;
+import com.oracle.jrockit.jfr.InvalidValueException;
 
-	public MeuArray(int size) {
-		objects = new MeuArray [size];	
+/**
+ * Class that represents my own array.
+ * 
+ * Internally it's has internal limitations but
+ * 
+ * when the limits has been exausthed the intenal limit is dobled.
+ * 
+ * @author abella
+ *
+ */
+public class MeuArray {
+
+	/*
+	 * Crie exceções para InvalidIndexException
+	 * Aplicar InvalidValueException nos outros métodos
+	 */
+	private int tamanho;
+	
+	/**
+	 * Constructor for my own array.
+	 * 
+	 * @param tamanho the initial size for my array
+	 */
+	public MeuArray(int tamanho) {
+		this.tamanho = tamanho;
 	}
 	
-	public static void addMeuArray (int size){
+	/**
+	 * Add an element for my array 
+	 * 
+	 * @param o element to be added 
+	 * @throws InvalidValueException 
+	 */
+	public void add(Object o) throws InvalidValueException {
 		
-		int pos = 0;
+		if(o == null) {
+			throw new InvalidValueException();
+		}
 		
 		
+		//TODO implementação do método
+	}
+	
+	/**
+	 * Remove an element for my array 
+	 * 
+	 * @param o element to be removed 
+	 */
+	public void remove(Object o) {
+		
+	}
+	
+	/**
+	 * Remove an element for my array by index
+	 * 
+	 * @param index the index of element that will be removed
+	 */
+	public void remove(int index) {
+		
+	}
+	
+	/**
+	 * Size of array
+	 * 
+	 * @return size of array
+	 */
+	public int size() {
+		return 0;
+	}
+	
+	/**
+	 * Convert my own array to native array
+	 * 
+	 * @return the converted native array
+	 */
+	public Object[] toArray() {
+		return null;
+	}
+	
+	/**
+	 * Prints all the elements
+	 */
+	public void listar() {
+		//TODO imprimir todos os elementos da lista
+	}
+	
+	/**
+	 * Get the element by index 
+	 * @param index index of element 
+	 * @return searched element
+	 */
+	public Object get(int index) {
+		return null;
 	}
 	
 }
---
+++ ListaSequencial/src/cesed/facisa/si/p3/listaSequencial/TesteMeuArray.java
@@ -0,0 +1,14 @@
+package cesed.facisa.si.p3.listaSequencial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteMeuArray {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
