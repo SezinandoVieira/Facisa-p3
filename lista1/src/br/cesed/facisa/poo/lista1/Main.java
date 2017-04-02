e9fbec10100e413490dc4a797b70f02a8e32f2be
Sezinando Vieira <Sezinando Vieira>
2017-04-02 20:42:22 -0300

    Adicionando respostas da Lista1.2

---
+++ lista1/src/br/cesed/facisa/poo/lista1/Main.java
@@ -0,0 +1,14 @@
+package br.cesed.facisa.poo.lista1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Questao5 ex1 = new Questao5("POO");
+		
+		Questao5 ex2 = new Questao5("POO", 30);
+		
+		
+		
+	}
+}
---
+++ lista1/src/br/cesed/facisa/poo/lista1/Questao5.java
@@ -0,0 +1,28 @@
+package br.cesed.facisa.poo.lista1;
+
+public class Questao5 {
+
+	private String nome;
+	
+	private int codigo;
+
+	/**
+	 * Constructor from class Questao5.java
+	 */
+	public Questao5(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	/**
+	 * Constructor from class Questao5.java
+	 */
+	public Questao5(String nome, int codigo) {
+		super();
+		this.nome = nome;
+		this.codigo = codigo;
+	}
+	
+	
+	
+}
