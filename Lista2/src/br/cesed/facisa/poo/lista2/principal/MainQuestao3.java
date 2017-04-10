14fc9c336dc7ba885fede46750d30a166358b95b
Sezinando Vieira <Sezinando Vieira>
2017-04-10 08:31:34 -0300

    Corrigindo exemplos

--- Lista2/src/br/cesed/facisa/poo/lista2/principal/MainQuestao3.java
+++ Lista2/src/br/cesed/facisa/poo/lista2/principal/MainQuestao3.java
@@ -6,11 +6,11 @@ public class MainQuestao3 {
 
 	public static void main(String[] args) {
 		
-		Fornecedor f1 = new Fornecedor("Biladas Inc","Rua da Torneirona Mole", "25665-8458", 5000.0);
+		Fornecedor f1 = new Fornecedor("Fornecedora 1","Rua João Pereira", "25665-8458", 5000.0);
 		
 		System.out.println(f1);
 		
-		Fornecedor f2 = new Fornecedor("Jailson Mendes ltda.", "Rua do Macho Peludo", "24241111-2568", 9000.0, 7000.0);
+		Fornecedor f2 = new Fornecedor("Fornecedora 2", "Rua Antonio Pereira", "24241111-2568", 9000.0, 7000.0);
 
 		System.out.println(f2);
 	}
