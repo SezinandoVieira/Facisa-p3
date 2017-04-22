83221e57a7f5ebd6b894ff3a3b16b40424fa1737
Sezinando Vieira <Sezinando Vieira>
2017-04-22 17:37:15 -0300

    Adicionando a resolução do exercicio de array

---
+++ exercicioArray/src/br/cesed/facisa/si/p3/arrays/entidades/ArrayListFacisa.java
@@ -0,0 +1,119 @@
+package br.cesed.facisa.si.p3.arrays.entidades;
+
+/**
+ * Class for my own implementation of ArrayList
+ * 
+ * @author Sezinando Vieira
+ *
+ */
+public class ArrayListFacisa {
+
+	private Object[] arrayInterno;
+
+	private int elementosInseridos;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param tamanho
+	 *            the initial size of ArrayList
+	 */
+	public ArrayListFacisa(int tamanho) {
+		this.arrayInterno = new Object[tamanho];
+	}
+
+	/**
+	 * Return the size of ArrayList.
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return elementosInseridos;
+	}
+
+	/**
+	 * Add an object
+	 * 
+	 * @param obj
+	 */
+	public void add(Object obj) {
+
+		if (elementosInseridos >= arrayInterno.length) {
+
+			Object[] arrayMaior = new Object[arrayInterno.length * 2];
+
+			for (int i = 0; i < arrayInterno.length; i++) {
+
+				arrayInterno[i] = arrayMaior[i];
+			}
+			arrayInterno = arrayMaior;
+		}
+
+		arrayInterno[elementosInseridos++] = obj;
+	}
+
+	/**
+	 * Remove an object
+	 * 
+	 * @param obj
+	 */
+	public void remove(Object obj) {
+		int elementoPosicao = findPosition(obj);
+
+		if (elementoPosicao != -1) {
+			for (int i = elementoPosicao; i < elementosInseridos; i++) {
+				arrayInterno[i] = arrayInterno[i + 1];
+			}
+		}
+		elementosInseridos--;
+	}
+
+	/**
+	 * Get the object by index.
+	 * 
+	 * For example, if I pass index as 0, this method will return the persisted
+	 * value at first position of Array.
+	 * 
+	 * @param index
+	 *            index in Array
+	 */
+	public Object getByIndex(int index) {
+
+		Object obj = null;
+
+		if (index < arrayInterno.length) {
+			obj = arrayInterno[index];
+		}
+
+		return obj;
+	}
+
+	/**
+	 * Check the position of the informed object.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public int findPosition(Object obj) {
+
+		int posicionamento = -1;
+
+		if (obj != null && arrayInterno.length > 0) {
+
+			for (int i = 0; i < arrayInterno.length;) {
+				posicionamento = i;
+				break;
+			}
+		}
+
+		return posicionamento;
+	}
+
+	public void printAllElements() {
+		for (Object objeto : arrayInterno) {
+			System.out.println(objeto);
+		}
+
+	}
+
+}
---
+++ exercicioArray/src/br/cesed/facisa/si/p3/arrays/teste/TesteArray.java
@@ -0,0 +1,34 @@
+package br.cesed.facisa.si.p3.arrays.teste;
+
+import br.cesed.facisa.si.p3.arrays.entidades.ArrayListFacisa;
+
+public class TesteArray {
+
+	public static void main(String[] args) {
+		
+		ArrayListFacisa alf = new ArrayListFacisa(3);
+		
+		alf.add("Objeto 1");
+		System.out.println(alf.size()); //deve retornar 1
+		
+		alf.add("Objeto 2");
+		System.out.println(alf.size()); //deve retornar 2
+		
+		alf.add("Objeto 3");
+		System.out.println(alf.size()); //deve retornar 3
+		
+		alf.add("Objeto 4"); //deve criar outro array internamente e repassar
+		System.out.println(alf.size()); //deve retornar 4
+		
+		System.out.println(alf.getByIndex(1)); //deve imprimir "Objeto 2"
+		
+		System.out.println(alf.findPosition("Objeto 2")); //deve retornar 1 
+		
+		alf.remove("Objeto 2");
+		System.out.println(alf.size()); //deve retornar 3
+		
+		alf.printAllElements();
+		
+	}
+
+}
