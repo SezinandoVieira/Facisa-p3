8d97fe2d43ad55759546ba96587f965ab4f7e821
Aluno <aluno@cesed.local>
2017-04-24 11:34:54 -0300

    Adcionando minha propia implementação da classe deque

---
+++ exercicioDeque/src/br/cesed/unifacisa/si/p3/deque/Deque.java
@@ -0,0 +1,51 @@
+package br.cesed.unifacisa.si.p3.deque;
+
+public class Deque {
+
+private int elementosInseridos;
+	
+	private Object [] dequeInterno;
+	
+	
+	
+	public Deque (int tamanho){
+		
+		this.dequeInterno = new Object [tamanho];
+	}
+	
+	public void addLast(Object obj) {
+			
+		if(elementosInseridos == dequeInterno.length){
+			this.duplica();
+		}
+		dequeInterno[elementosInseridos++] = obj;
+		
+	}
+	
+	public void addFirst(Object obj){
+		if(elementosInseridos == dequeInterno.length){
+			this.duplica();
+		}
+		Object [] dequeNovo = new Object[dequeInterno.length];
+		dequeNovo[0]=obj;
+		for (int i = 1; i >= this.elementosInseridos ; i++){
+			dequeNovo[i] = dequeInterno[i-1];
+		}
+		dequeInterno = dequeNovo;
+		elementosInseridos++;
+	}
+	
+	private void duplica(){
+		if(elementosInseridos == dequeInterno.length){
+			Object [] dequeNovo = new Object[dequeInterno.length * 2];
+			for ( int i = 0; i < elementosInseridos; i++){
+				
+				dequeNovo[i] = dequeInterno[i];
+				
+			}
+		
+			dequeInterno = dequeNovo;
+		}
+	}
+	
+}
