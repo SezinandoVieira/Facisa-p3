5ff5bb6abb1b16a3f0db8ecb906b8af804dba34a
Aluno <aluno@cesed.local>
2017-04-24 10:33:47 -0300

    Adicionando o tratamento para dobrar a pilha, caso o seu tamanho incial não seja sufuciente

--- exercicioArray/src/br/cesed/facisa/si/p3/pilha/Pilha.java
+++ exercicioArray/src/br/cesed/facisa/si/p3/pilha/Pilha.java
@@ -1,47 +1,61 @@
 package br.cesed.facisa.si.p3.pilha;
-
+/**
+ * 
+ * @author Sezinando
+ * 
+ * Class for my own implementation of stack
+ *
+ */
 public class Pilha {
 
-	private Object  [] pilhaInterna;
-	
+	private Object[] pilhaInterna;
+
 	private int pos;
 
 	/**
 	 * Constructor from class Pilha.java
 	 */
 	public Pilha(int tamanho) {
-		
-		this.pilhaInterna = new Object [tamanho];
-		
+
+		this.pilhaInterna = new Object[tamanho];
+
 	}
-	
+
 	/**
 	 * Responsible method for remove the top element
 	 * 
 	 */
-	public Object pop(){
+	public Object pop() {
 		pilhaInterna[--pos] = null;
-		return  this.top();
+		return this.top();
 	}
-	
+
 	/**
 	 * 
 	 * Responsible method to insert one element in the top
 	 * 
 	 */
-	
-	public void push(Object obj){
-		if (obj != null){
-			pilhaInterna[pos++]=obj;
-		}		
+
+	public void push(Object obj) {
+		if (pos == pilhaInterna.length) {
+
+			Object[] pilhaNova = new Object[pilhaInterna.length * 2];
+			for (int i = 0; i < pilhaNova.length; i++) {
+				pilhaInterna[i] = pilhaNova[i];
+			}
+			pilhaInterna = pilhaNova;
+
+		}
+		pilhaInterna[pos++] = obj;
+
 	}
+
 	/**
 	 * 
 	 * Responsible method to consult the top element
 	 */
-	public Object top(){
-		return pilhaInterna[pos-1];
+	public Object top() {
+		return pilhaInterna[pos - 1];
 	}
-	
-	
+
 }
